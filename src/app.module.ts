a8ab383a4271662941085cc1b67e35a4c8b38680
AmRaiy <m.ilhamraisubagja27@gmail.com>
2025-11-20 11:43:43 +0700

    dne

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,14 @@
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
+import { KategoriJangkaWaktuModule } from './kategori-jangka-waktu/kategori-jangka-waktu.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
+    KategoriJangkaWaktuModule, // <-- tambahkan ini
   ],
   providers: [PrismaService],
 })
 export class AppModule {}
-
-
