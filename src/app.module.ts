dfd6c5bdfc4ecb327abbc0861e60d73644066237
syahdan312 <indrasyahdan530@gmail.com>
2025-11-21 14:06:47 +0700

    jabatan

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 ﻿import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
+import { JabatanModule } from './jabatan/jabatan.module'; // ← Tambahkan ini
+
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
+    JabatanModule, // ← Tambahkan di sini
   ],
   providers: [PrismaService],
 })
 export class AppModule {}
-
-
