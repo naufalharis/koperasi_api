20c1a5ff74436ec2828b66c235309c0380761e8a
alzanmizam <alfauzanadzan81@gmail.com>
2025-11-24 13:08:13 +0700

    anjay

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
-﻿// src/app.module.ts
-import { Module } from '@nestjs/common';
+﻿import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-
 import { AuthModule } from './auth/auth.module';
-import { JabatanModule } from './jabatan/jabatan.module'; // ← Tambahkan ini
-
 import { PrismaService } from './prisma/prisma.service';
+import { PinjamanModule } from './pinjaman/pinjaman.module';
+import { KategoriJangkaWaktuModule } from './kategori-jangka-waktu/kategori-jangka-waktu.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
-    PinjamanModule, // <--- WAJIB ADA
+    PinjamanModule,
+    KategoriJangkaWaktuModule,
+     // <--- Tambahkan ini
   ],
   providers: [PrismaService],
 })
