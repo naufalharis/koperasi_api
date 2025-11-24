a0a262840817b988a9780dec99332c042a8b1b4e
naufalharis <naufalharis1012@gmail.com>
2025-11-24 13:14:33 +0700

    anjaii

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,19 @@
 ﻿import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { JabatanModule } from './jabatan/jabatan.module'; // ← Tambahkan ini
 
-import { PrismaService } from './prisma/prisma.service';
-import { PinjamanModule } from './pinjaman/pinjaman.module';
+import { AuthModule } from './auth/auth.module';
+import { JabatanModule } from './jabatan/jabatan.module';
 import { KategoriJangkaWaktuModule } from './kategori-jangka-waktu/kategori-jangka-waktu.module';
+import { KategoriSimpananModule } from './simpanan/kategori-simpanan.module'; // ← Tambahkan ini
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
+    JabatanModule,
+    KategoriJangkaWaktuModule,
+    KategoriSimpananModule
   ],
   providers: [PrismaService],
 })
